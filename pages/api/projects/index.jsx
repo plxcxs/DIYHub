79b07470fd27612b4b856e8d86137a5eff23e6ad
sebastian <plxcxs@protonmail.com>
2026-09-04 14:28:42 +0200

    added api enpoint

---
+++ pages/api/projects/index.jsx
@@ -0,0 +1,16 @@
+import dbConnect from "@/db/connect";
+import Project from "@/db/models/Project";
+
+export default async function handler(request, response) {
+  if (request.method === "GET") {
+    try {
+      await dbConnect();
+      const projects = await Project.find();
+      return response.status(200).json(projects);
+    } catch (error) {
+      return response.status(500).json({ message: "error fetching projects" });
+    }
+  } else {
+    return response.status(405).json({ message: "method not allowed" });
+  }
+}
