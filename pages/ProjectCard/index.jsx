304382374db93e2d56f80a58fef9e5c4db1b5666
sebastian <plxcxs@protonmail.com>
2026-09-07 13:29:13 +0200

    added all data to display on the card

--- pages/ProjectCard/index.jsx
+++ pages/ProjectCard/index.jsx
@@ -1,1 +1,20 @@
-export default function ProjectCard() {}
+import Image from "next/image";
+export default function ProjectCard({
+  title,
+  image,
+  description,
+  complexity,
+  duration,
+}) {
+  return (
+    <>
+      <section>
+        <h3>{title} </h3>
+        <Image alt="image" href={image} />
+        <p>{description} </p>
+        <div>{complexity}</div>
+        <div>{duration}</div>
+      </section>
+    </>
+  );
+}
--- pages/ProjectList/index.js
+++ pages/ProjectList/index.js
@@ -1,13 +1,26 @@
 import useSWR from "swr";
+import ProjectCard from "../ProjectCard";
 export default function ProjectList() {
   const fetcher = (...args) =>
     fetch(...args).then((response) => response.json());
   const { data: projects, error, isLoading } = useSWR("/api/projects", fetcher);
-
+  if (error) return <p>Error loading projects.</p>;
+  if (isLoading) return <p>Loading...</p>;
   console.log({ projects, error, isLoading });
   return (
     <>
-      <section>hello</section>
+      {projects.map((project) => {
+        return (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            image={project.imageUrl}
+            duration={project.duration}
+            complexity={project.complexity}
+          />
+        );
+      })}
     </>
   );
 }
