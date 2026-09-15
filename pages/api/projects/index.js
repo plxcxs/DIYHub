70dbdc6b3f8166402d00f87219e48a9973f8aae5
sebastian <plxcxs@protonmail.com>
2026-09-15 17:14:48 +0200

    refactored api/projects

--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -2,22 +2,20 @@ import dbConnect from "@/db/connect";
 import Project from "@/db/models/Project";
 
 export default async function handler(request, response) {
-  try {
-    await dbConnect();
-  } catch (error) {
-    console.error("DB Connection Error:", error);
-    return response.status(500).json({ message: "database connection failed" });
-  }
   if (request.method === "GET") {
     try {
+      await dbConnect();
       const projects = await Project.find().sort({ _id: -1 });
       return response.status(200).json(projects);
     } catch (error) {
       console.error("API Error:", error);
       return response.status(500).json({ message: "error fetching projects" });
     }
-  } else if (request.method === "POST") {
+  }
+
+  if (request.method === "POST") {
     try {
+      await dbConnect();
       const projectData = request.body;
       const newProject = await Project.create(projectData);
       return response.status(201).json(newProject);
@@ -25,7 +23,6 @@ export default async function handler(request, response) {
       console.error("API Error:", error);
       return response.status(400).json({ error: error.message });
     }
-  } else {
-    return response.status(405).json({ message: "method not allowed" });
   }
+  return response.status(405).json({ message: "method not allowed" });
 }
